d5bbc1b9cf66b65d5fb3be99c39807345acd33f3
LuisRodriguez-rr25017 <rr25017@ues.edu.sv>
2026-06-19 14:25:51 -0600

    Update App.test.js

--- Entrega_3_final/frontend/src/App.test.js
+++ Entrega_3_final/frontend/src/App.test.js
@@ -3,9 +3,8 @@ import App from './App';
 
 test('Comprueba que la interfaz cargue y muestre el titulo del sistema', () => {
   render(<App />);
-  
-  // Buscamos alguna palabra clave que sepamos que esta en su interfaz visual.
-  // "Biblioteca" es casi segura, pero si falla, puedes cambiarla por "Socios" o "Libros"
+
+         
   const textoEnPantalla = screen.getByText(/Biblioteca/i);
   
   expect(textoEnPantalla).toBeInTheDocument();
